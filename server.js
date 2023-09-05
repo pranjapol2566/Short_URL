3a21f3955bb2ab036d4607257b192aaf66b37c4a
pranjapol2566 <pranjapolsuriyachai@gmail.com>
2023-09-06 00:15:23 +0700

    success Short_URL

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const ShortUrl = require('./models/shortUrl')
 const app = express()
 
+mongoose.connect('mongodb+srv://admin:1234@cluster0.meesrfk.mongodb.net/', {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
+
 app.set('view engine','ejs')
+app.use(express.urlencoded({ extended: false }))
 
 
-app.get('/', (req, res) => {
-    res.render('index')
+app.get('/', async (req, res) => { 
+    const shortUrls = await ShortUrl.find()
+    res.render('index', { shortUrls: shortUrls})
+})
+
+app.post('/shortUrls', async (req, res) => {
+   await ShortUrl.create({ full: req.body.fullUrl})
+   res.redirect('/')
+})
+
+app.get('/:id', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.id})
+  if(shortUrl == null) return res.sendStatus(404)
+
+  shortUrl.clicks++
+  shortUrl.save()
+
+  res.redirect(shortUrl.full)
 })
 
 app.listen(process.env.PORT || 5000);
