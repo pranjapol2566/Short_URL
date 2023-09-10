679734372492d0cbb2b1499e1a674d494b93b55d
pranjapol2566 <pranjapolsuriyachai@gmail.com>
2023-09-10 12:19:56 +0700

    Update server.js

--- server.js
+++ server.js
@@ -37,8 +37,9 @@ app.get('/:id', async (req, res) => {
 
     if (shortUrl == null) return res.sendStatus(404)
 
-    const ipAddress = req.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    shortUrl.ipAddress = ipAddress;
+    const ipAddress = req.ip
+
+    shortUrl.ipAddress = ipAddress
 
     shortUrl.clicks++
     shortUrl.save()
